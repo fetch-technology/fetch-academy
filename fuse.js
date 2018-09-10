315c66b5d897869d9bf9e7b4ad832e063ba30e49
Tung Dao <me@tungdao.com>
2018-09-10 10:16:48 +0700

    init project

---
+++ fuse.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const fb = {
+  FuseBox, CopyPlugin, QuantumPlugin,
+  LESSPlugin, CSSPlugin,
+} = require('fuse-box')
+
+const production = process.env.NODE_ENV === 'production'
+      || process.env.ENV === 'production'
+      || process.argv.includes('--production')
+
+const fuse = FuseBox.init({
+  homeDir: 'frontend',
+  output: 'public/$name.js',
+  useTypescriptCompiler: true,
+  plugins: [
+    production && QuantumPlugin({
+      bakeApiIntoBundle: 'app',
+      treeshake: true,
+      uglify: true
+    })
+  ]
+})
+
+const bundle = fuse.bundle('app')
+  .instructions('> main.jsx')
+
+if (!production) {
+  fuse.dev({ port: 4444, fallback: 'index.html' })
+  bundle.hmr().watch()
+}
+
+fuse.run()
