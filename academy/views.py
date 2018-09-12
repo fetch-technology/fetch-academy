2f41c8f556d82ab4ea077602c01013f414deb99b
dangqhuy <dangqhuy2411@gmail.com>
2018-09-12 09:57:03 +0700

    done

--- academy/views.py
+++ academy/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from . import serializers
+from . import models
 
-# Create your views here.
+
+class UserLessonAPIView(generics.ListAPIView):
+    serializer_class = serializers.UserLessonSerializer
+
+    def get_queryset(self):
+        print(self.kwargs)
+        return models.UserLesson.objects.filter(student=self.kwargs['pk'])
