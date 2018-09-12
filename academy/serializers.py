bf324295882e25277859c6b0b450e949f5f497ab
TaiPtit <vuductaiptit@gmail.com>
2018-09-12 15:49:27 +0700

    using depth option instead of specific fileds in  serializer

--- academy/serializers.py
+++ academy/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.User
-        fields = '__all__' 
+        exclude = ['password'] 
 
         
 class CourseSerializer(serializers.ModelSerializer):
@@ -26,26 +26,12 @@ class CourseSerializer(serializers.ModelSerializer):
 
 
 class UserCourseSerializer(serializers.ModelSerializer):
-    course_title = serializers.SerializerMethodField(source="get_course_title")
-    begin = serializers.SerializerMethodField(source="get_begin")
-    end = serializers.SerializerMethodField(source="get_end")
-    mentor_name = serializers.SerializerMethodField(source="get_mentor_name")
 
     class Meta:
         model = models.Participation
-        fields = ['course_title', 'begin', 'end', 'mentor_name', 'course'] 
-    
-    def get_course_title(self, model):
-        return model.course.title
+        fields = '__all__'
+        depth = 2
 
-    def get_end(self, model):
-        return model.course.end
-
-    def get_begin(self, model):
-        return model.course.begin
-
-    def get_mentor_name(self, model):
-        return model.course.mentor.get_full_name() 
   
 class DetailUserLessonsSerializers(serializers.ModelSerializer):
     class Meta:
