6e0b81d9efbefbdaf06cdc321b6739307659dc85
TaiPtit <vuductaiptit@gmail.com>
2018-09-12 10:52:52 +0700

    edited admin for Course model

--- academy/admin.py
+++ academy/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from . import models
 
+
+class ParticipationInline(admin.TabularInline):
+    model = models.Participation
+    extra = 1
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = (ParticipationInline,)
+
+    
 admin.site.register(models.Program)
-admin.site.register(models.Course)
-admin.site.register(models.Participant)
+admin.site.register(models.Course, CourseAdmin)
+admin.site.register(models.Participation)
 admin.site.register(models.Lesson)
 admin.site.register(models.UserLesson)
