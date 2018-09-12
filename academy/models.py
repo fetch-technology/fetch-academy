0835d76871b16faa2a7e2cebf09d92c7ae3144a5
dangqhuy <dangqhuy2411@gmail.com>
2018-09-12 14:40:37 +0700

    change double quote to single quote

--- academy/models.py
+++ academy/models.py
@@ -17,9 +17,9 @@ class Program(BaseModel):
 
 class Course(BaseModel):
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
-    mentor = models.ForeignKey(User, related_name="courses", on_delete=models.CASCADE)
+    mentor = models.ForeignKey(User, related_name='courses', on_delete=models.CASCADE)
     title = models.CharField(_('Title'), max_length=250)
-    students = models.ManyToManyField(User, through="Participation")
+    students = models.ManyToManyField(User, through='Participation')
     begin = models.DateTimeField(_('Begin'), auto_now=True)
     end = models.DateTimeField(_('End'), blank=True, null=True)
 
@@ -29,7 +29,7 @@ class Course(BaseModel):
 
 class Participation(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, related_name="participants", on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name='participants', on_delete=models.CASCADE)
     begin = models.DateTimeField(_('Begin'), auto_now=True)
     end = models.DateTimeField(_('End'), blank=True, null=True)
 
@@ -46,8 +46,8 @@ class Lesson(BaseModel):
         )
     title = models.CharField(_('Title'), max_length=250)
     content = models.TextField(_('Content'))
-    program = models.ForeignKey( Program, related_name="lessons", on_delete=models.CASCADE)
-    students = models.ManyToManyField(User, through="UserLesson")
+    program = models.ForeignKey( Program, related_name='lessons', on_delete=models.CASCADE)
+    students = models.ManyToManyField(User, through='UserLesson')
     
     def __str__(self):
         return self.title
