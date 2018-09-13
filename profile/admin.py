2c19ee84766438c07d8b9d06a51a8e0b66850aa5
dangqhuy <dangqhuy2411@gmail.com>
2018-09-13 09:56:29 +0700

    add profile to team (inline admin)

--- profile/admin.py
+++ profile/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
+
+
+class ProfileInline(admin.TabularInline):
+    model = models.Profile
+
+
+@admin.register(models.Team)
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [
+        ProfileInline,
+    ]
+
 admin.site.register(models.Profile)
-admin.site.register(models.Team)
