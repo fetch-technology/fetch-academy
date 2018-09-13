0958526aae1d6d7a2290eb9c883d24df7eb6c7fb
Nam Tran <trantrungnam1997vn2@gmail.com>
2018-09-13 13:37:51 +0700

    Update CourseOverview

--- frontend/containers/CourseOverview.js
+++ frontend/containers/CourseOverview.js
@@ -29,7 +29,7 @@ export default class CourseOverview extends React.Component {
                     <p className='mb-4'>
                       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
                       been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-                       a galley of type and scrambled it to make a type specimen ab book.
+                       a galley of type and scrambled it to make a type specimen a book.
                     </p>
                   </div>
                 </div>
