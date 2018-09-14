8bec16f09203a6412ab1f7d5df9e80df6a7f1413
dangqhuy <dangqhuy2411@gmail.com>
2018-09-14 14:55:45 +0700

    remove parameter of API user_lessons

--- academy/urls.py
+++ academy/urls.py
@@ -5,9 +5,9 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter(trailing_slash=False)
 router.register('courses', views.CourseViewset, base_name='courses')
-router.register('user_courses/(?P<user>[^/.]+)', views.UserCourseAPIView, base_name='user-courses')
+router.register('user_courses', views.UserCourseAPIView, base_name='user-courses')
 
 urlpatterns = [
-    path('api/v1/lessons/<int:pk>/<int:program>', views.UserLessonAPIView.as_view(), name='user_lessons'),
+    path('api/v1/lessons/<int:program>', views.UserLessonAPIView.as_view(), name='user_lessons'),
     path('api/v1/', include(router.urls)),
 ]
