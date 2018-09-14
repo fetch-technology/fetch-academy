3fd5d79def6cd620fb4e6e1a0a35033529248e22
dangqhuy <dangqhuy2411@gmail.com>
2018-09-14 14:34:22 +0700

    login singup with access token

--- profile/serializers.py
+++ profile/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
+import requests
 from . import models
 
 
@@ -12,17 +13,28 @@ class UserSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         User = get_user_model()
+        userinfo = requests.get('https://www.googleapis.com/oauth2/v3/userinfo', headers={
+            'Authorization': 'Bearer {access_token}'.format(**validated_data)
+        }).json()
+
         try:
-            self.instance = User.objects.get(username=validated_data['username'])
-            return self.instance
+            self.instance = User.objects.get(email=userinfo['email'])
         except User.DoesNotExist:
-            return super().create(validated_data)
+            self.instance = User.objects.create_user(
+                userinfo['email'], 
+                email=userinfo['email'],
+                first_name=userinfo['given_name'],
+                last_name=userinfo['family_name']
+            )
+
+        login(self.context.get('request'), self.instance)
+        return self.instance
+
 
     def is_valid(self, raise_exception=False):
         self._validated_data = self.initial_data
         self._errors = {}
         return True
-
 
 
 class ProfileSerializer(serializers.ModelSerializer):
