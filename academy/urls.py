a307d81faeb143f973678707f34242e684f4126a
Đặng Hồ Vũ <dangvanminh09@gmail.com>
2018-09-14 17:57:15 +0700

    fix urls

--- academy/urls.py
+++ academy/urls.py
@@ -8,7 +8,7 @@ router.register('courses', views.CourseViewset, base_name='courses')
 router.register('user-courses', views.UserCourseViewset, base_name='user-courses')
 
 urlpatterns = [
-    path('api/v1/lessons/<int:program>', views.UserLessonAPIView.as_view(), name='user_lessons'),
-    path('api/v1/lesson/<int:pk>', views.DetailLessonAPIView.as_view(), name='detail_lesson'),
+    path('api/v1/programs/<int:program>/lessons', views.UserLessonAPIView.as_view(), name='user_lessons'),
+    path('api/v1/lessons/<int:pk>', views.DetailLessonAPIView.as_view(), name='detail_lesson'),
     path('api/v1/', include(router.urls)),
 ]
