ac7dbef87e2e8d765ee52c0f9df27e141deed0e2
Đặng Hồ Vũ <dangvanminh09@gmail.com>
2018-09-14 11:46:14 +0700

    change url to path

--- academy/urls.py
+++ academy/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
-from django.conf.urls import url
 from . import views
 from rest_framework.routers import DefaultRouter
 
 
-router = DefaultRouter(trailing_slash=True)
+router = DefaultRouter(trailing_slash=False)
 router.register('courses', views.CourseViewset, base_name='courses')
 router.register('user_courses/(?P<user>[^/.]+)', views.UserCourseAPIView, base_name='user-courses')
 
 urlpatterns = [
     path('api/v1/lessons/<int:pk>/<int:program>', views.UserLessonAPIView.as_view(), name='user_lessons'),
-    url('api/v1/', include(router.urls)),
+    path('api/v1/', include(router.urls)),
 ]
